6395935b39dd838795d5c73ef0fd4bdaddcc1c88
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-06-12 20:36:51 -0500

    updated RGB 2 HSV and fixed minor update to verify

--- ImageProcessing_DataAug/color_space_test.py
+++ ImageProcessing_DataAug/color_space_test.py
@@ -5,19 +5,23 @@ from PIL import Image
 
 
 # 3. Perform the Conversion and generate image
-def rgb_to_hsv(img, hue, sat, val):
+def rgb_to_hsv(img):
    rgb_img = img.convert('RGB')
-   r,g,b = rgb_img.getpixel((1,1))
+   img_arry = np.array(rgb_img)
+   # This make sure we are working with [0,1] for Saturation and Value
+   img_arry = img_arry/255.0
 
    # Value
    V = np.max(img, axis=2)
    # Chroma
    C = V - np.min(img, axis=2)
    # Saturation
-   S = 0
-   if V.all:
+   if V != 0:
       S = C/V
+   else:
+      S = 0
    
+   r, g, b = rgb_img[..., 0], rgb_img[..., 1], rgb_img[..., 2]
    # Hue
    H = 0
    if V.all == r:
@@ -26,10 +30,9 @@ def rgb_to_hsv(img, hue, sat, val):
       H = ((b-r)/C)+2
    elif V.all == b:
       H = ((r-g)/C)+4 
-   Hue = H*60
+   Hue = H * 60.0
+   hsv = np.stack([Hue, S, V], axis=2)
 
-   hsv = np.stack([Hue, S, V], axis=1)
-   
 def hvs_to_rgb(img, hue, sat, val):
    
    # chrome = VxS
@@ -40,11 +43,11 @@ def hvs_to_rgb(img, hue, sat, val):
 # 1. First verify user argument are valid
 def verify(hue, sat, val):
    if hue < 0 or hue > 360:
-      raise ValueError("Hue")
+      raise ValueError("Hue, must be between [0, 360]")
    if sat < 0 or sat > 1:
-      raise ValueError("Saturation")
+      raise ValueError("Saturation, must be between [0, 1.0]")
    if val < 0 or val > 1:
-      raise ValueError("Value")
+      raise ValueError("Value, must be between [0, 1.0]")
    return 1 
     
 if __name__ =="__main__":
@@ -60,11 +63,5 @@ if __name__ =="__main__":
 
    # 2. Read the image from the path
    img = Image.open(img_path)
-
-   # Display the image in a GUI, NOTE: only used for debugging
-   #cv2.imshow('Image', img)
-   #cv2.waitKey(0) 
-   #cv2.destroyAllWindows()
-
    rgb_to_hsv(img, hue, sat, val)
    #hvs_to_rgb(img, hue, sat, val)
