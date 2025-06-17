34482beffc1cfdc407f4091a43e1e4e678eadf0f
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-06-16 22:16:46 -0500

    debugging function extract patch

--- ImageProcessing_DataAug/img_transforms.py
+++ ImageProcessing_DataAug/img_transforms.py
@@ -1,10 +1,11 @@
 import numpy as np
+from numpy.lib import stride_tricks
 from PIL import Image
 
 """Random Cropping images"""
-def random_crop(img_arry, S):
+def random_crop(img, S):
    # Optain the Size of an image
-   width,height,_ = img_arry.shape
+   width,height,_ = img.shape
 
    # Check Size is valid value
    if not 0 < S or not S <= min(width,height):
@@ -23,17 +24,36 @@ def random_crop(img_arry, S):
    left_right = rand_x - size_w
    top_bottom = rand_y - size_w
 
-   return img_arry[top_bottom : top_bottom + S, left_right : left_right + S]
+   return img[top_bottom : top_bottom + S, left_right : left_right + S]
 
+"""Patch Extraction"""
+def extract_patch(img):
+   # non-overlapping patches of size 8
+   size = 8
+   H, W,_ = img.shape
+   shape = [H // size, W // size] + [size, size]
+
+   # (row, col, patch_row, patch_col)
+   strides = [size * s for s in img.strides] + list(img.strides)
+   # extract patches
+   patches = stride_tricks.as_strided(img, shape=shape, strides=strides)
+   return patches
 
    
 if __name__ == "__main__":
 
-   # 1st. Random Cropping
    img = Image.open("original.jpg")
+   
+   # 1st. Random Cropping
    img_arry = np.array(img)
    S = 200
-
    crop_img = random_crop(img_arry, S)
+   
    new_img = Image.fromarray(crop_img)
-   new_img.save("crop_Img.jpg")
+   #new_img.save("crop_Img.jpg")
+
+   # 2nd. Patch Extraction
+   patch_img = extract_patch(img_arry)
+      
+   new_img = Image.fromarray(patch_img)
+   new_img.save("patch_Img.jpg")
