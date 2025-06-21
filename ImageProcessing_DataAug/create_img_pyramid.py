ca422a2bcc24325b18bd9065640c5a3722ff41a2
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-06-20 20:16:36 -0500

    created a file that makes image pyramid from original to smallest

--- ImageProcessing_DataAug/create_img_pyramid.py
+++ ImageProcessing_DataAug/create_img_pyramid.py
@@ -3,11 +3,19 @@ from PIL import Image
 
 
 def img_pyramid(img, py_h):
-   print("Hello")
+   # Straight forword, take the original image size and 
+   # divide by 2 for every py_h = number of time user want us to divide image repetative.
+   H,W,_ = img.shape
+   new_img = Image.fromarray(img)
+   numImg = 2
 
-
-
-
+   while py_h != 1:
+      py_h = py_h-1
+      H = H//2
+      W = W//2
+      resized_image = new_img.resize((W, H))  # (width, height)
+      resized_image.save(f"img_{numImg}x.png")
+      numImg = numImg*2
 
 img = Image.open("original.jpg")
 img_array = np.array(img)
