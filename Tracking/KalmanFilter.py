c69c37378832dcab6100ced37ec280a6d5e0e913
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-07-19 22:26:06 -0500

    Kalman predict works

--- Tracking/KalmanFilter.py
+++ Tracking/KalmanFilter.py
@@ -1,50 +1,40 @@
+
 import numpy as np
 
 class KalmanFilter:
-    def __init__(self, initial_state, dt=1.0):
-        # State: [x, y, vx, vy]
-        self.state = initial_state
+    """
+    A simple Kalman filter for tracking 2D position and velocity.
+    State vector x = [px, py, vx, vy].
+    """
+    def __init__(self, init_pos, dt=1.0, accel_variance=1.0, meas_variance=1.0):
         self.dt = dt
-        
-        # State transition matrix
-        self.F = np.array([
-            [1, 0, dt, 0],
-            [0, 1, 0, dt],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
-        
-        # Process covariance
-        self.Q = np.eye(4) * 0.01
-        
-        # Measurement matrix (only position observable)
-        self.H = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0]
-        ])
-        
-        # Measurement covariance
-        self.R = np.eye(2) * 0.1
-        
-        # State covariance
-        self.P = np.eye(4) * 1.0
-        
+        self.F = np.array([[1, 0, dt, 0],
+                           [0, 1, 0, dt],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+        self.H = np.array([[1, 0, 0, 0],
+                           [0, 1, 0, 0]])
+        q = accel_variance
+        G = np.array([[dt**2/2, 0],
+                      [0, dt**2/2],
+                      [dt, 0],
+                      [0, dt]])
+        self.Q = G @ G.T * q
+        self.R = np.eye(2) * meas_variance
+        self.P = np.eye(4) * 500.
+        self.x = np.array([init_pos[0], init_pos[1], 0., 0.], dtype=float)
+
     def predict(self):
-        self.state = self.F @ self.state
+        self.x = self.F @ self.x
         self.P = self.F @ self.P @ self.F.T + self.Q
-        return self.state[:2]  # Return predicted position
-        
-    def update(self, measurement):
-        # Measurement: [x, y]
-        y = measurement - self.H @ self.state
+        return self.x[:2]
+
+    def update(self, y):
+        y = np.array(y)
+        a = y - (self.H @ self.x)
         S = self.H @ self.P @ self.H.T + self.R
         K = self.P @ self.H.T @ np.linalg.inv(S)
-        
-        self.state = self.state + K @ y
-        self.P = (np.eye(4) - K @ self.H) @ self.P
-        
-    def get_position(self):
-        return self.state[:2]
-    
-    def get_velocity(self):
-        return self.state[2:]
+        self.x = self.x + K @ a
+        I = np.eye(self.F.shape[0])
+        self.P = (I - K @ self.H) @ self.P
+        return self.x[:2]
